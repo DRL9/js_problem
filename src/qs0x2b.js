c735e8ec359ee4b8df3cf0e2f3ea46f311d30b2e
Yuanjun Zhong <937676516@qq.com>
2023-07-23 09:40:11 +0800

    feat: 添加注释

--- src/qs0x2b.js
+++ src/qs0x2b.js
@@ -6,7 +6,7 @@ async function test() {
     let resp = await axios.get('http://localhost:3000/?txt=' + txt);
     let data = resp.data;
     // koa 的 ctx.query 使用 querystring.parse + 会变成空格
-    console.log(data); // output: me w
+    console.log('koa 解析: ' + data); // output: me w
     // 同样 + (0x2b) 变成空格
     /**
      * @see https://url.spec.whatwg.org/#urlencoded-parsing
@@ -15,11 +15,11 @@ async function test() {
      *
      * 所以，+ 记得用 url encode
      */
-    console.log(new URLSearchParams('a=' + txt).get('a')); // output: me w
+    console.log('URLSearchParams 解析: ', new URLSearchParams('a=' + txt).get('a')); // output: me w
 
     resp = await axios.get('http://localhost:3000/?txt=' + encodeURIComponent(txt));
     data = resp.data;
-    console.log(data); // output: me+w
+    console.log('进行了encodeURIComponent: ' + data); // output: me+w
 
     process.exit(0);
 }
